c820c77240ca32ee505b6a9c50428828f86285c7
Rohit K. Chatterjee <strategy.chatterjee@gmail.com>
2014-09-17 18:18:27 +0000

    AzureML integration

--- e1071.r
+++ e1071.r
@@ -1,6 +1,17 @@
 library("e1071")
-training_data=array(data=c(10,9,9,10,11,10,11,11,9,10), dim=c(10,1))
-model <- svm(training_data, factor(rep(1,dim(training_data)[1])), scale=TRUE, type="one-classification")
-model["SV"]
-model["index"]
-model["coefs"]
+
+# dataset1 <- maml.mapInputPort(1) # class: data.frame
+# training_data=array(data=dataset1, dim=c(5,2))
+
+training_data=array(data=c(10,9,9,10,11,10,11,11,9,10), dim=c(5,2))
+model <- svm(training_data, 
+			factor(rep(1,dim(training_data)[1])), 
+			scale=TRUE, 
+			type="one-classification")
+
+data.coefs=data.frame(model["coefs"], model["SV"], model["index"], model["x.scale"])
+
+validation_data=array(data=c(10,10,9,9,8,8), dim=c(3,2))
+preds<-predict(model, validation_data)
+
+# maml.mapOutputPort("data.coefs");
